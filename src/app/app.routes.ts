ab6d818ba45063ba70a9e24cf5028a8f7d8ac994
Ansif <ansif.pi@csrinformatik.de>
2024-09-30 11:54:28 +0530

    Added components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+import { NgModule } from '@angular/core';
+
+// Declare and export routes so they can be used elsewhere
+export const routes: Routes = [
+  { path: '', component: HomeComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
