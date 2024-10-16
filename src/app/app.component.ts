eae17298703805f1d1d8208cd38bf09a986730fc
Bineesh123-coder <bineeshmbslx@gmail.com>
2024-10-16 11:42:04 +0530

    navigation bar error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component'; // Import HomeComponent
 import { CounterComponent } from './counter/counter.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HomeComponent,CounterComponent]
+  imports: [HomeComponent,CounterComponent, RouterModule]
 })
 export class AppComponent {
   title = 'Angular Day3';
@@ -16,7 +17,15 @@ export class AppComponent {
 
   handleButtonClick(event: string) {
     this.outputMessage = event;
-    // alert(event);
+    alert(event);
+  }
+
+  count = 0; // Initial count for two-way binding
+
+  // Handle count changes emitted from child (Two-way binding)
+  handleCountChange(newCount: number) {
+    this.count = newCount; // Update the count in the parent component
+    alert(newCount);
   }
   
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter, Route } from '@angular/router';
+import { HomeComponent } from './app/home/home.component'
+import { CounterComponent } from './app/counter/counter.component';
+
+
+const routes : Route[] = [
+  { path: 'home', component: HomeComponent },
+  { path: 'counter', component: CounterComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
+
 
 bootstrapApplication(AppComponent)
   .catch((err) => console.error(err));
