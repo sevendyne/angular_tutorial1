8e059183d388517655e5d0ac57ea9c7bbeaa0aad
fajr07 <fajrk16032002@gmail.com>
2024-10-16 11:38:24 +0530

    comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,31 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from './home/home.component'; // Import HomeComponent
+import { HomeComponent } from './home/home.component'; 
+import { CounterComponent } from './counter/counter.component';
+
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HomeComponent]
+  imports: [HomeComponent, CounterComponent, RouterModule]
 })
 export class AppComponent {
-  title = 'Angular Day3';
+  title = 'Hello Angular';
   outputMessage: string = '';
 
   handleButtonClick(event: string) {
     this.outputMessage = event;
-    // alert(event);
+    alert(event);
+  }
+
+  count = 0; // Initial count for two-way binding
+
+  // Handle count changes emitted from child (Two-way binding)
+  handleCountChange(newCount: number) {
+    this.count = newCount; // Update the count in the parent component
+    alert(newCount);
   }
 }
+
